1c16c6d890b453495582626631d9626df572c049
gkubiak92 <gkubiak92@gmail.com>
2020-05-05 21:18:33 +0200

    Move api config to untracked file

---
+++ src/data/api-config.js
@@ -0,0 +1,9 @@
+export default {
+  apiKey: "AIzaSyBmAw5icBgtYvdW1IbHs1kIZjjz-W4B2GI",
+  authDomain: "clothing-shop-98b58.firebaseapp.com",
+  databaseURL: "https://clothing-shop-98b58.firebaseio.com",
+  projectId: "clothing-shop-98b58",
+  storageBucket: "clothing-shop-98b58.appspot.com",
+  messagingSenderId: "250387665223",
+  appId: "1:250387665223:web:8916e2fdc8eab29c27fc62",
+};
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,16 +1,7 @@
 import firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/auth";
-
-const config = {
-  apiKey: "AIzaSyBmAw5icBgtYvdW1IbHs1kIZjjz-W4B2GI",
-  authDomain: "clothing-shop-98b58.firebaseapp.com",
-  databaseURL: "https://clothing-shop-98b58.firebaseio.com",
-  projectId: "clothing-shop-98b58",
-  storageBucket: "clothing-shop-98b58.appspot.com",
-  messagingSenderId: "250387665223",
-  appId: "1:250387665223:web:8916e2fdc8eab29c27fc62",
-};
+import config from "../data/api-config";
 
 firebase.initializeApp(config);
 
