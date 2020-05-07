bcebbe5d3c662ccba17cd221dde172aa75b8f613
gkubiak92 <gkubiak92@gmail.com>
2020-05-07 21:10:56 +0200

    Add add to cart button

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import "./collection-item.styles.scss";
+import CustomButton from "../custom-button/custom-button.component";
 
 export default function CollectionItem({ imageUrl, name, price }) {
   return (
@@ -13,6 +14,7 @@ export default function CollectionItem({ imageUrl, name, price }) {
         <span className="name">{name}</span>
         <span className="price">{price}</span>
       </div>
+      <CustomButton classes="inverted">ADD TO CART</CustomButton>
     </div>
   );
 }
