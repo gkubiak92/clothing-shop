42c98072560edf88c8a953fd2f7e904a59f8af53
gkubiak92 <gkubiak92@gmail.com>
2020-05-20 21:43:15 +0200

    Add saga for user login persistance

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -77,4 +77,13 @@ export const convertCollectionsSnapshotToMap = (collections) => {
   }, {});
 };
 
+export const getCurrentUser = () => {
+  return new Promise((resolve, reject) => {
+    const unsubscribe = auth.onAuthStateChanged((userAuth) => {
+      unsubscribe();
+      resolve(userAuth);
+    }, reject);
+  });
+};
+
 export default firebase;
